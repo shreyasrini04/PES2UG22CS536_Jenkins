a307b17179f1e843f85a24f01235d343232cc6dd
shreyasrini04 <148338730+shreyasrini04@users.noreply.github.com>
2025-03-17 19:28:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "I am Shreya-PES2UG22CS536 \n";
   return 0;
 }
